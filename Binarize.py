cf89d3d4f7845d4e2d2ea04681f759de5358ed5f
konstantin <lihota200000@gmail.com>
2023-05-13 15:30:52 +0300

    Исправленны ошибки в алгоритмах, добавлены данные и метрики

--- Binarize.py
+++ Binarize.py
@@ -1,17 +1,17 @@
-from enum import Enum
 import numpy as np
 from scipy.stats import norm
 import cv2
+from Metrics import *
 
 
-class BinarizationMethod(Enum):
+class BinarizationMethod():
     NIBLACK = 'Niblack'
     OTSU = 'Otsu'
     OTSU_L = 'Otsu_l'
     NIBLACK_MULTISCALE = 'Niblack_multiscale'
 
 
-def binarize(image, method, mask, window_size, min_window_size, max_window_size, metric, k, a):
+def binarize(image, method, mask, window_size, min_window_size, max_window_size, k, a):
     if method == BinarizationMethod.NIBLACK:
         bin_image = niblack_threshold(image, window_size, k, a)
     elif method == BinarizationMethod.OTSU:
@@ -19,31 +19,42 @@ def binarize(image, method, mask, window_size, min_window_size, max_window_size,
     elif method == BinarizationMethod.OTSU_L:
         bin_image = maximum_likelihood_thresholding(image)
     elif method == BinarizationMethod.NIBLACK_MULTISCALE:
-        bin_image = adaptive_niblack_threshold(image, mask, min_window_size, max_window_size, metric, k, a)
+        bin_image = adaptive_niblack_threshold(image, mask, min_window_size, max_window_size, k, a)
     else:
         raise ValueError('Unknown binarization method')
     return bin_image
 
-def otsu_thresholding(image):
 
+def otsu_thresholding(image):
     hist, bins = np.histogram(image, bins=256, range=(0, 255))
     n_pixels = np.sum(hist)
 
     norm_hist = hist / n_pixels
 
-    weights = np.cumsum(norm_hist)
-    means = np.cumsum(np.arange(256) * norm_hist)
+    # mean = np.mean(img)
 
-    global_mean = means[-1]
+    # Вычисляем межклассовую дисперсию для каждого порога
+    max_var = 0
+    threshold = 0
+    for i in range(256):
 
-    variances = ((global_mean * weights - means)**2) / (weights * (1 - weights))
+        w0 = np.sum(norm_hist[:i])
+        w1 = np.sum(norm_hist[i:])
 
-    threshold = np.argmax(variances)
+        u0 = np.sum(norm_hist[:i] * np.arange(i)) / w0 if w0 > 0 else 0
+        u1 = np.sum(norm_hist[i:] * np.arange(i, 256)) / w1 if w1 > 0 else 0
 
-    return threshold
+        var = w0 * w1 * (u0 - u1) ** 2
+
+        if var > max_var:
+            max_var = var
+            threshold = i
+    mask = np.where(image > threshold, 1, 0)
+
+    return mask.astype(np.uint8) * 255
+
 
 def maximum_likelihood_thresholding(image):
-
     hist, bins = np.histogram(image, bins=256, range=(0, 255))
     n_pixels = np.sum(hist)
     group1 = hist[:128]
@@ -51,17 +62,17 @@ def maximum_likelihood_thresholding(image):
 
     mean1 = np.sum(np.arange(128) * group1) / np.sum(group1)
     mean2 = np.sum(np.arange(128, 256) * group2) / np.sum(group2)
-    var1 = np.sum((np.arange(128) - mean1)**2 * group1) / np.sum(group1)
-    var2 = np.sum((np.arange(128, 256) - mean2)**2 * group2) / np.sum(group2)
+    var1 = np.sum((np.arange(128) - mean1) ** 2 * group1) / np.sum(group1)
+    var2 = np.sum((np.arange(128, 256) - mean2) ** 2 * group2) / np.sum(group2)
 
     likelihood1 = norm.pdf(np.arange(128), mean1, np.sqrt(var1))
     likelihood2 = norm.pdf(np.arange(128, 256), mean2, np.sqrt(var2))
 
-    criterion = np.sum(np.log(likelihood1 * np.sum(group1) / n_pixels + likelihood2 * np.sum(group2) / n_pixels))
-
     threshold = np.argmax(likelihood1 * np.sum(group1) / n_pixels + likelihood2 * np.sum(group2) / n_pixels)
+    mask = image > threshold
 
-    return threshold
+    return mask.astype(np.uint8) * 255
+
 
 def niblack_threshold(img, window_size, k, a=10):
     integral_img = np.cumsum(np.cumsum(img, axis=0), axis=1)
@@ -85,24 +96,26 @@ def niblack_threshold(img, window_size, k, a=10):
     area = window_size ** 2
     mean = sum_ / area
     var = (sum_sq - mean ** 2) / area
-
     threshold = mean + k * np.sqrt(var) - a
     mask = img > threshold
 
     return mask.astype(np.uint8) * 255
 
-def adaptive_niblack_threshold(img, img_label, min_window_size=2, max_window_size=500, metric=cv2.PSNR, k=0.2, a=10):
-    best_ql = 0
-    best_window = min(img.shape)
-    best_bin_img = niblack_threshold(img, best_window, k)
 
-    for window_size in range(min_window_size, max_window_size, 10):
+def adaptive_niblack_threshold(img, img_label, min_window_size=10, max_window_size=500, k=0.2, a=10):
+    best_ql = 0
+    metric = PSNR
+    m_len =np.max(img.shape)
+    step = (max_window_size - min_window_size)//20
+    for window_size in range(min_window_size, max_window_size, step):
+
         bin_img = niblack_threshold(img, window_size, k, a)
         qality = metric(bin_img, img_label)
         if best_ql < qality:
             best_bin_img = bin_img
             best_ql = qality
-            #best_window = window_size
+            # best_window = window_size
+        if window_size>m_len:
+            break
 
     return best_bin_img
-
--- Metrics.py
+++ Metrics.py
@@ -1,25 +1,113 @@
 from enum import Enum
 import cv2
+import numpy as np
+from math import sqrt
 
 
-class MetricsMethod(Enum):
-    DRD = 'DRD'
-    PSNR = 'PSNR'
+
+
+
+def Creat_W(n):
+    m = 2 * n + 1
+    i_c = (m - 1) // 2
+    j_c = (m - 1) // 2
+    W = np.array(
+        [[1 / sqrt((i - i_c) ** 2 + (j - j_c) ** 2) if (abs(j - j_c) + abs(i - i_c)) != 0 else 0 for i in range(m)] for
+         j in range(m)])
+    W[i_c][j_c] = 0
+    W_sum = np.sum(W)
+    W = W / W_sum
+    return W
+
+
+def DRDblock(i, j, W, m, g, f):
+    xf = f.shape[0]
+    yf = f.shape[1]
+    value_gk = g[i][j]
+    Bk = np.zeros((m, m))
+    Dk = np.zeros((m, m))
+    h = m
+    for x in range(m - 1):
+        for y in range(m - 1):
+            if (i - h + x < 1 or j - h + y < 1 or i - h + x > xf or j - h + y > yf):
+                Bk[x][y] = value_gk
+            else:
+                Bk[x][y] = f[i - h + x - 1][j - h + y - 1];
+            Dk[x][y] = abs(Bk[x][y] - value_gk)
+    DRDk = np.sum(Dk * W)
+    return DRDk
+
+
+def nubm_calc(f, ii, jj, blck):
+    startx = (ii - 1) * blck + 1
+    endx = ii * blck
+    starty = (jj - 1) * blck + 1
+    endy = jj * blck
+    check_prv = -2
+    retb = 0
+    for xx in range(startx, endx - 1):
+        for yy in range(startx, endx - 1):
+            check = f[xx - 1][yy - 1]
+            if (check_prv < 0):
+                check_prv = check
+            elif (check != check_prv):
+                retb = 1
+                break
+        if (retb != 0):
+            break
+    return retb
+
+
+def DRD( pred,gt, n=2, SIZE_BLOCK=8):
+    W = Creat_W(n)
+    if np.max(pred) != 1:
+        pred[pred < 127] = 0
+        pred[pred > 127] = 1
+    if np.max(gt) != 1:
+        gt[gt < 127] = 0
+        gt[gt > 127] = 1
+
+    # assert(set(gt)==2, set(pred)==2)
+    error_map = np.abs(gt.astype('int32') - pred.astype('int32'))  # //255
+    error_padded = np.pad(error_map, ((n, n), (n, n)), mode='constant', constant_values=0)
+    DRDk_sum = 0
+    NUBM = 0
+
+    for i in range(n, error_map.shape[0] + n):
+        for j in range(n, error_map.shape[1] + n):
+            if error_padded[i][j] == 1:
+                DRDk_sum += np.sum(error_padded[i - n:i + n + 1, j - n:j + n + 1] * W)
+
+    for i in range(0, gt.shape[0] // SIZE_BLOCK * SIZE_BLOCK, SIZE_BLOCK):
+
+        for j in range(0, gt.shape[1] // SIZE_BLOCK * SIZE_BLOCK, SIZE_BLOCK):
+
+            if np.sum(gt[i:i + SIZE_BLOCK, j:j + SIZE_BLOCK]) != SIZE_BLOCK * SIZE_BLOCK and np.sum(
+                    gt[i:i + SIZE_BLOCK, j:j + SIZE_BLOCK]) != 0:
+                NUBM += 1
+
+    if NUBM == 0:
+        NUBM = 1
+    return DRDk_sum/NUBM
 
 
 def PSNR(image, label):
-    return cv2.PSNR(image, label)
 
+    mse = np.mean((image.astype(np.float64) / 255 - label.astype(np.float64) / 255) ** 2)
+    if mse == 0:
+        return 100
+    return 10 * np.log10(1. / mse)
 
-class Metrics:
-    def __init__(self, metric_name):
-        self.metric = cv2.PSNR
-        if metric_name == MetricsMethod.PSNR:
-            self.metric = PSNR
-        # elif metric_name == MetricsMethod.DRD:
+def ignore_area(image, label):
+     label[label == 128] = image[label == 128]
+     return label
+def Metrics(metric_name, image, label):
+    label = ignore_area(image, label)
 
-    def get_metric(self):
-        return self.metric
-
-    def slove_metric(self, image, label):
-        return self.metric(image, label)
+    if metric_name == 'PSNR':
+        metric = PSNR(image, label)
+    elif metric_name == 'DRD':
+        metric = DRD(image, label)
+    else:
+        metric = None
+    return  metric
